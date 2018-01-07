622c2960fd7fd54a42b4724f437792ca6e4eb5e5
mehdi <mehdisouid1993@gmail.com>
2018-01-07 05:59:28 +0100

    update  de  rapport

--- backend/src/main/java/com/marketplace/service/CommentaireService.java
+++
@@ -1,7 +0,0 @@
-package com.marketplace.service;
-
-import com.marketplace.domain.Commentaire_Article;
-
-public interface CommentaireService {
-public void saveCommentaire(Commentaire_Article com);
-}
